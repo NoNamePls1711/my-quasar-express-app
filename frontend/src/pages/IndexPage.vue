871cf070712aaec17aaab2c8e9336e01c11b2448
Sarayut380 <zusuchang@gmail.com>
2026-01-20 15:04:44 +0700

    feat: integrate frontend with backend using axios and env config

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,15 +1,26 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="q-pa-md">
+    <h4>API Demo</h4>
+
+    <q-btn label="Load Data" color="primary" @click="loadData" />
+
+    <div v-if="data" class="q-mt-md">
+      <p><strong>Git:</strong> {{ data.git }}</p>
+      <p><strong>Docker:</strong> {{ data.docker }}</p>
+    </div>
   </q-page>
 </template>
 
 <script setup>
-defineOptions({
-  name: 'IndexPage'
-});
+import { ref } from 'vue'
+import axios from 'axios'
+
+const data = ref(null)
+
+const loadData = async () => {
+  const res = await axios.get(
+    `${import.meta.env.VITE_API_URL}/api/demo`
+  )
+  data.value = res.data
+}
 </script>
